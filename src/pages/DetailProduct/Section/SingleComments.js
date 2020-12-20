117cf4a266379466f0c2734efb68fe640645cb61
congpt <20160500@student.hust.edu.vn>
2020-12-20 23:25:06 +0700

    edit detail product

--- src/pages/DetailProduct/Section/SingleComments.js
+++ src/pages/DetailProduct/Section/SingleComments.js
@@ -2,27 +2,29 @@ import React from 'react';
 import {Comment, Avatar, Tooltip ,Button, Input} from 'antd';
 import moment from 'moment';
 
+import { UserOutlined } from '@ant-design/icons';
 
 function SingleComments(props) {
 
     return (
         <div>
             <Comment
-                author={props.comment.name}
+                author={
+                    <p style={{fontSize: 15, color: "#161515",fontWeight:'bold'}}>
+                        {props.comment.name}
+                    </p>
+                }
                 avatar={
-                    <Avatar
-                        src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-                        alt="image"
-                    />
+                    <Avatar shape="square" size={64} icon={<UserOutlined />} />
                 }
                 content={
-                    <p>
+                    <p style={{fontSize: 20, color: "#161515"}}>
                         {props.comment.content}
                     </p>
                 }
                 datetime={
                     <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
-                        <span>{moment().fromNow()}</span>
+                        <span style={{fontSize: 15}}>{moment().fromNow()}</span>
                     </Tooltip>
                 }
             ></Comment>
